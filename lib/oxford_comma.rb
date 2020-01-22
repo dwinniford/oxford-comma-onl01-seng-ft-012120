560aad4cbd69c8bfabd06dca5f34c2a87b5ad239
Dave Winniford <dwinniford@gmail.com>
2020-01-22 22:31:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,14 @@
 def oxford_comma(array)
+  case array.length 
+  when 1 
+    do comma_string = array[1]
+    comma_string
+  when 2 
+    do comma_string = array.join(" and ")
+    comma_string
+  when >= 3 
+    do 
  comma_string = array.slice(0..-2).join(", ") + ", and " + array[-1]
  comma_string
+  end  
 end
