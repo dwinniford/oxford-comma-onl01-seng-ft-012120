e32ef056aca467b081c39f543b8c5e29b5a79c18
Dave Winniford <dwinniford@gmail.com>
2020-01-22 22:34:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   size = array.size 
     if size == 1 
-      comma_string = array[1]
+      comma_string = array[0]
       comma_string
     elsif size == 2 
       comma_string = array.join(" and ")
