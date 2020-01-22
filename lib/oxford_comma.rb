db5b6f90f8e607123463f29cbb5937ec706170f9
Dave Winniford <dwinniford@gmail.com>
2020-01-22 22:34:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,13 @@
 def oxford_comma(array)
-  case array.length 
-  when 1 
-    do comma_string = array[1]
-    comma_string
-  when 2 
-    do comma_string = array.join(" and ")
-    comma_string
-  when >= 3 
-    do 
- comma_string = array.slice(0..-2).join(", ") + ", and " + array[-1]
- comma_string
-  end  
+  size = array.size 
+    if size == 1 
+      comma_string = array[1]
+      comma_string
+    elsif size == 2 
+      comma_string = array.join(" and ")
+      comma_string
+    if size >= 3 
+      comma_string = array.slice(0..-2).join(", ") + ", and " + array[-1]
+      comma_string
+    end  
 end
